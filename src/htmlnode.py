9be32813363476f203afbd2f56bfd5f72761e2dd
Andy <andymucyo@gmail.com>
2026-04-09 10:53:28 +0200

    added LeafNode class and tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -21,3 +21,30 @@ class HTMLNode:
 
     def __repr__(self) -> str:
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag: str | None = None, value: str | None = None, props: dict[str, str] | None = None):
+        super().__init__(tag = tag, value = value, props= props)
+
+    def props_to_html(self):
+        return super().props_to_html()
+
+    def to_html(self) -> str:
+        if self.value is None:
+            raise ValueError("Error: this node has no value")
+
+        if self.tag is None:
+            return self.value
+
+        if self.props is not None:
+            if self.tag == "img":
+                return f"<{self.tag}{self.props_to_html()} />"
+
+            if self.tag == "a":
+                return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+        return f"<{self.tag}>{self.value}</{self.tag}>"
+
+    def __repr__(self) -> str:
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
---
+++ src/test_leafnode.py
@@ -0,0 +1,43 @@
+import unittest
+
+from htmlnode import LeafNode
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node: LeafNode = LeafNode(
+                tag= "p",
+                value= "Test paragraph"
+                )
+        self.assertEqual(node.to_html(), "<p>Test paragraph</p>")
+
+    def test_leaf_to_html_a(self):
+        node: LeafNode = LeafNode(
+                tag= "a",
+                value= "click here",
+                props= {"href": "https://www.google.com"} 
+                )
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">click here</a>')
+
+    def test_leaf_to_html_img(self):
+        node: LeafNode = LeafNode(
+                tag= "img",
+                value= "image",
+                props= {"src": "image.jpg", "alt":"description"} 
+                )
+        self.assertEqual(node.to_html(), '<img src="image.jpg" alt="description" />' )
+
+    def test_leaf_repr(self):
+        node: LeafNode = LeafNode(
+                tag= "a",
+                value= "click here",
+                props= {"href":"https://www.google.com"} 
+                )
+        self.assertEqual(repr(node), "LeafNode(a, click here, {'href': 'https://www.google.com'})")
+
+    def test_leaf_value_error(self):
+        node: LeafNode = LeafNode(tag= "p")
+
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+
