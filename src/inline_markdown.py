47ea7b69af55fcc2b21cdc0d4fa7308eacdfc0f5
Andy <andymucyo@gmail.com>
2026-04-23 16:46:59 +0200

    uncommented debug tests

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -103,14 +103,14 @@ def text_to_textnodes(text: str) -> list[TextNode]:
 
 
 if __name__ == "__main__":
-    # node = TextNode(
-    #     "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png) blablabla",
-    #     TextType.TEXT,
-    # )
-    #
-    # new_nodes = split_nodes_images([node])
-    #
-    # print(new_nodes)
+    node = TextNode(
+        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png) blablabla",
+        TextType.TEXT,
+    )
+
+    new_nodes = split_nodes_images([node])
+
+    print(new_nodes)
 
     text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
     print(text_to_textnodes(text))
