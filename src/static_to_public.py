e98e1ec676de26982bf20787eaa714b41c78801a
Andy <andymucyo@gmail.com>
2026-04-29 19:11:01 +0200

    enhanced static_to_public function

--- src/static_to_public.py
+++ src/static_to_public.py
@@ -1,17 +1,20 @@
 import os
 import shutil
 
-def static_to_public(source: str = "static", destination: str = "public", root: str = ".") :
+def static_to_public(source: str = "static", destination: str = "public", is_root: bool = True) :
     destination_path: str = os.path.abspath(destination)
     source_path: str = os.path.abspath(source)
-    root_path: str = os.path.abspath(root)
 
     if os.path.exists(destination_path) and os.path.isdir(destination_path):
-        if destination == "public": 
-            shutil.rmtree(os.path.join(root_path, destination))
+        if is_root: 
+            print(f"Deleting {destination} directory...")
+            shutil.rmtree(destination_path)
             os.mkdir(destination)
+            print("Copying static files to public directory...")
     else:
+        print(f"Creating {destination} directory")
         os.mkdir(destination)
+        print("Copying static files to public directory...")
     to_copy: list[str] = os.listdir(source_path)
     for item in to_copy:
         item_path: str = os.path.join(source_path, item)
@@ -20,9 +23,8 @@ def static_to_public(source: str = "static", destination: str = "public", root: 
         else:
             new_dir = os.path.join(destination, item)
             new_dir_path: str = os.path.join(destination_path, item)
-            if not os.path.exists(new_dir_path):
-                os.mkdir(new_dir_path)
-            static_to_public(source=item_path, destination=new_dir, root=source)
+            os.mkdir(new_dir_path)
+            static_to_public(source=item_path, destination=new_dir, is_root=False)
 
 
 
