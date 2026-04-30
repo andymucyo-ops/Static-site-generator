d95603f1f09055d6cfd3fc761644d65271acf8d1
Andy <andymucyo@gmail.com>
2026-04-30 16:53:33 +0200

    feature: generate_file function & test files

--- src/general_page.py
+++ src/general_page.py
@@ -1,16 +1,42 @@
 
+import os
 import textwrap
 from src.block_markdown import markdown_to_html_node
-from src.htmlnode import HTMLNode
+from src.htmlnode import HTMLNode, ParentNode
 
 
 def extract_title(markdown: str) -> str:
     html: HTMLNode = markdown_to_html_node(markdown)
     for node in html.children:
        if node.tag == "h1":
-            title_content: list[str] = [s.value for s in node.children]
-            return "".join(title_content).strip()
+           title_node: ParentNode = html.children.pop(html.children.index(node))
+           title_content: list[str] = [s.value for s in title_node.children]
+           return "".join(title_content).strip()
     raise Exception("no Title found")
+
+def generate_page(from_path: str, template_path: str, dest_path: str) -> None:
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    from_path = os.path.abspath(from_path)
+    with open(from_path, "r") as f:
+        src_file_content: str = f.read()
+
+    template_path = os.path.abspath(template_path)
+    with open(template_path, "r") as f:
+        template_content: str = f.read()
+
+    src_as_html: str=  markdown_to_html_node(src_file_content).to_html()
+    title: str = extract_title(src_file_content)
+    
+    template_content = template_content.replace("{{ Title }}", title)
+    template_content = template_content.replace("{{ Content }}", src_as_html)
+
+    # print(template_content)
+    dest_path = os.path.abspath(dest_path)
+    with open(dest_path, "w") as f:
+        f.write(template_content)
+
+
+
 
 
 if __name__ == "__main__":
@@ -42,5 +68,7 @@ if __name__ == "__main__":
     #     print(block_to_block_type(block))
     # print(md)
     # print(markdown_to_html_node(md).to_html())
-    print(extract_title(md))
+    
+    # print(extract_title(md))
+    generate_page("content/index.md", "template.html", "public/index.md")
 
